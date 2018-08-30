804911d35c74e898dee1c5612380c1ef2dfdf048
Isaac <isaacjardim@hotmail.com>
2018-08-29 22:19:44 -0300

    Aula 6 - Atividade 1

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import AutorBox from './Autor';
+import Home from './Home';
+import './index.css';
+import {BrowserRouter as Router, Route,Switch} from 'react-router-dom';
+
+ReactDOM.render(
+  (<Router>
+    <App>
+    <Switch>
+    <Route exact path="/" component={Home}/>
+    <Route path="/autor" component={AutorBox}/>
+    <Route path="/livro" component={AutorBox}/>
+    </Switch>
+    </App>
+    </Router>),
+  document.getElementById('root')
+);
